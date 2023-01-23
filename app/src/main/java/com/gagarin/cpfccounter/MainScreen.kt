4f06821f2919ecc10228b5ec8eaf817e82ed85a3
GagarinAndrei <gagarin1982@gmail.com>
2023-01-23 16:44:39 +0500

    Add foodlist

--- app/src/main/java/com/gagarin/cpfccounter/MainScreen.kt
+++ app/src/main/java/com/gagarin/cpfccounter/MainScreen.kt
@@ -1,15 +1,23 @@
 package com.gagarin.cpfccounter
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -21,7 +29,11 @@ import com.gagarin.cpfccounter.ui.theme.CPFCCounterTheme
 
 @Composable
 fun MainScreen() {
-    CPFCOverview()
+    Column {
+        CPFCOverview()
+        Spacer(modifier = Modifier.height(16.dp))
+        FoodsList()
+    }
 }
 
 @Composable
@@ -62,33 +74,69 @@ fun CPFCOverview() {
 fun OverviewElement(item: String) {
 //    var maxValue by remember { StateOf("0") }
 //    var currentValue by remember { mutableStateOf("0") }
-
-    Row(
-        modifier = Modifier
-            .padding(8.dp)
-            .fillMaxWidth()
-    ) {
-        Text(
-            text = item,
-            modifier = Modifier.weight(2f)
-        )
-        Spacer(modifier = Modifier.size(4.dp))
-        Text(
-            text = "currentValue",
+    Column {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = item,
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.size(4.dp))
+            Text(
+                text = "currentValue",
 //            modifier = Modifier.weight(1f)
-        )
-        Spacer(modifier = Modifier.size(4.dp))
-        Text(
-            text = "maxValue",
+            )
+            Spacer(modifier = Modifier.size(4.dp))
+            Text(
+                text = "maxValue",
 //            modifier = Modifier.weight(1f)
+            )
+        }
+        LinearProgressIndicator(
+            progress = 1.0f,
+            modifier = Modifier.fillMaxWidth()
         )
     }
+}
+
+@Composable
+fun FoodsList() {
+    val list = remember {
+        List(10) {
+            "Item ${it + 1}"
+        }.toMutableStateList()
+    }
+    LazyColumn(
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        items(list) { item ->
+            FoodListItem(
+                foodName = item,
+            )
+        }
+
+    }
+}
+
+@Composable
+fun FoodListItem(foodName: String) {
+    Text(
+        text = foodName,
+        modifier = Modifier.fillMaxWidth()
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun MainScreenPreview() {
     CPFCCounterTheme {
-        CPFCOverview()
+        MainScreen()
     }
 }
